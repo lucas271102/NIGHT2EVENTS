b64b42eedbc3eaed5fb6c5d7242c806d065a49eb
lucas271102 <lucas001spe@gmail.com>
2023-06-26 00:12:54 -0300

    home

--- src/components/CatAndLogoNavbar.jsx
+++ src/components/CatAndLogoNavbar.jsx
@@ -1,16 +1,125 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-
+import logo from '../assets/png/logo-no-background.png'
 const CatAndLogoNavbar = ()=>{
     return(
 
-<div className="bg-black flex justify-evenly ">
-    <h1 className="text-white">NIGHT2</h1>
-    <a className="text-white">Vender </a>
-  
+
    
-</div>
+<header className="bg-black">
+  <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+    <div className="flex h-16 items-center justify-between">
+      <div className="md:flex md:items-center md:gap-12">
+        <a className="block text-teal-600" href="/">
+          <span className="sr-only">Home</span>
+          <img src={logo} className='w-32 p-4 h-24' />
+        </a>
+      </div>
+
+      <div className="hidden md:block">
+        <nav aria-label="Global">
+          <ul className="flex items-center gap-6 text-sm">
+            <li>
+              <a
+                className="text-white transition hover:text-gray-500/75"
+                href="/"
+              >
+                About
+              </a>
+            </li>
+
+            <li>
+              <a
+                className="text-white transition hover:text-gray-500/75"
+                href="/"
+              >
+                Careers
+              </a>
+            </li>
+
+            <li>
+              <a
+                className="text-white transition hover:text-gray-500/75"
+                href="/"
+              >
+                History
+              </a>
+            </li>
+
+            <li>
+              <a
+                className="text-white transition hover:text-gray-500/75"
+                href="/"
+              >
+                Services
+              </a>
+            </li>
+
+            <li>
+              <a
+                class="text-white transition hover:text-gray-500/75"
+                href="/"
+              >
+                Projects
+              </a>
+            </li>
+
+            <li>
+              <a
+                class="text-white transition hover:text-gray-500/75"
+                href="/"
+              >
+                Blog
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="flex items-center gap-4">
+        <div class="sm:flex sm:gap-4">
+          <a
+            class="rounded-md bg-violet-700 px-5 py-2.5 text-sm font-medium text-white shadow"
+            href="/"
+          >
+            Login
+          </a>
+
+          <div class="hidden sm:flex">
+            <a
+              class="rounded-md bg-white px-5 py-2.5 text-sm font-medium text-black"
+              href="/"
+            >
+              Register
+            </a>
+          </div>
+        </div>
+
+        <div class="block md:hidden">
+          <button
+            class="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
     )
 }
 export default CatAndLogoNavbar
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import logo from '../assets/png/logo-no-background.png'
 const Footer = ( )=>{
     return(
 
-        <footer className="w-[100%] mt-auto py-4 bg-black text-white">Footer</footer>
+        <footer class="bg-black">
+        <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+          <div class="sm:flex sm:items-center sm:justify-between">
+            <div class="flex justify-center text-teal-600 sm:justify-start">
+            <img src={logo} className='w-32 h-24'/>
+            </div>
+      
+            <p class="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
+              Copyright &copy; 2023. All rights reserved. Created by Lucas Speziale
+            </p>
+          </div>
+        </div>
+      </footer>
     )
 }
 export default Footer
