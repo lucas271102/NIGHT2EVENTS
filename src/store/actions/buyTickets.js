575d06ffbb8daedfd372851b5cb2135ab36fe8fe
lucas271102 <lucas001spe@gmail.com>
2023-07-05 14:45:04 -0300

    reduxTickets

--- src/store/actions/buyTickets.js
+++ src/store/actions/buyTickets.js
@@ -7,8 +7,14 @@ async(response)=>{
    try {
     let token = localStorage.getItem('token')
     let headers = {headers:{'Authorization':`Bearer ${token}`}}
-    
+    let res = await axios(apiUrl+'tickets', headers)
+    console.log('response', res.data.tickets)
+    return {tickets:res.data.tickets}
    } catch (error) {
-    
+    console.log(error)
+    return{
+        tickets:[]
+    }
    }}
 )
+const actions = {buyTickets_read}
