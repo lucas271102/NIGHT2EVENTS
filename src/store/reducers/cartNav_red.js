3c1166538f03777ba2c764433f1d3c874fb4267e
lucas271102 <lucas001spe@gmail.com>
2023-07-21 16:37:01 -0300

    store

--- src/store/reducers/cartNav_red.js
+++ src/store/reducers/cartNav_red.js
@@ -1,5 +1,22 @@
 import { createReducer } from "@reduxjs/toolkit";
 import actions from '../actions/cartNav.js'
+const {cartNav_action }= actions
 let initialState={
     cart : []
 }
+const reducer = createReducer(
+    initialState,
+    (builder)=>{
+        builder.addCase(
+    cartNav_action.fulfilled,
+    (state, action)=>{
+        let newState={
+            ...state,
+            cart:action.payload.cart
+        }
+        return newState
+    }
+        )
+    }
+)
+export default reducer
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import user_LoginReducer from './reducers/user_Login.js'
 import buyTickets_reducer from './reducers/buyTickets_reducer.js'
+import cartNav_reducer from './reducers/cartNav_red.js'
 const store = configureStore({
     reducer:{
     userLogin : user_LoginReducer,
-    buyTickets:buyTickets_reducer
+    buyTickets:buyTickets_reducer,
+    navCartItemsCount:cartNav_reducer
     }
 })
 export default store
