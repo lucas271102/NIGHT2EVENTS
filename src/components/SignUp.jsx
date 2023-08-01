801adb507cd2828d401aea82baba78af87316d60
lucas271102 <lucas001spe@gmail.com>
2023-08-01 16:36:37 -0300

    users

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -31,7 +31,7 @@ const SignUp = ()=>{
         duration: 9000,
         isClosable: true,
       })
-      localStorage.setItem('user', data)
+      localStorage.setItem('user',JSON.stringify(data))
   
           navigate('/login')
     })
